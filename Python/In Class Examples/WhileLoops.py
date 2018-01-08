d48081b93a3472c2a6913e2303eec7d16fb74e3e
afeinsod <alfeinsod@gmail.com>
2018-01-08 15:32:52 -0800

    Create WhileLoops.py

---
+++ Python/In Class Examples/WhileLoops.py
@@ -0,0 +1,21 @@
+password = "Abracadabra"
+user = ""
+while (user !=password):
+  user = input("What's the magic word?")
+print ( "Correct!" )
+         
+print ( "Next example:")
+i=0
+while (i<5) :
+  print ( "My name is Alix! " )
+  print i
+  i+=1
+  
+print ( "Last example:")
+       
+i=0
+sum=0
+while (i<10) :
+  sum += i
+  print "Sum is: ", sum
+  i+=1
